9f573709aeb3c10b1bad803c1393958347fc3ba0
Tolib <abdukarimovta@nsk.ithub.ru>
2026-01-15 18:09:43 +0700

    Python first lesson

---
+++ lesson1.py
@@ -0,0 +1,79 @@
+4 # int
+print(type(4))
+2.34 # float
+print(type(2.34))
+0.00000000002
+2e-10 # float
+print(type(2e-10))
+'Hello world!'
+"Hello world!" # string
+print(type('Hello world!'))
+True
+False # bool
+print(type(True))
+'g'; "f" # string
+
+# Math oper
+a = 2 + 3
+b = 3 - 5
+c = 3 * 2
+d = 34 / 5
+print(d)
+d = 34 // 5
+print(d)
+d = 34 % 5
+print(d)
+f = 5 ** 2
+print(f)
+
+str1 = 'Hello '
+str2 = 'world!'
+print(str1 + str2)
+print(str1[2])
+
+# a = a + 2
+print(a)
+a += 2
+print(a)
+b -= 2
+c *= 2
+d /= 2
+
+# i++, i--, ++i, --i
+i = 0
+i += 1; i -= 1
+
+str3 = '''
+        gfhjkfhgf
+        fghfghgfhf
+        gfjghjhgj
+        '''
+print(type(str3))
+str4 = """
+fghlkjjfgh
+gfhgf
+hfghfghfg
+hgfhf
+"""
+
+a = (2 + 5) * 4
+b = (2 - 3) / (3 - 1) ** 2
+
+# fghgfhgjfhbhgfhbhb
+
+number = 5
+student_number = 21 # C# camelCase
+
+class MyClass:
+    is_public_field: int
+    _is_protected_field: float
+    __is_private_field: str
+
+print()
+print("fdhfg")
+
+def my_function(name: str) -> None:
+    print(f"Called my function! Hi, {name}")
+    return None
+
+my_function(6)
